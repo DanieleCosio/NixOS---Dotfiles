6e684dd2922acde66d44d724b4c8e3c1773b78c1
daniele.cosio <daniele.cosio99@gmail.com>
2024-02-04 09:49:51 +0100

    Mime apps

--- home.nix
+++ home.nix
@@ -136,6 +136,33 @@ with {
     components = [ "pkcs11" "secrets" "ssh" ];
   };
 
+  # Defaults apps
+  xdg = {
+    enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = [ "spacefm.desktop" ];
+        "application/pdf" = [ "google-chrome.desktop" ];
+        "application/x-extension-htm" = [ "google-chrome.desktop" ];
+        "application/x-extension-html" = [ "google-chrome.desktop" ];
+        "application/x-extension-shtml" = [ "google-chrome.desktop" ];
+        "application/x-extension-xht" = [ "google-chrome.desktop" ];
+        "application/x-extension-xhtml" = [ "google-chrome.desktop" ];
+        "application/x-extension-xhtml+xml" = [ "google-chrome.desktop" ];
+        "image/*" = [ "google-chrome.desktop" ];
+        "video/*" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/chrome" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/ftp" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/http" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/https" = [ "google-chrome.desktop" ];
+        "text/html" = [ "google-chrome.desktop" ];
+        "text/markdown" = [ "code.desktop" ];
+        "text/plain" = [ "code.desktop" ];
+      };
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
