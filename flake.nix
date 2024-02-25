179a9f926e148181def41390575d9a02b2542a0c
daniele.cosio <daniele.cosio99@gmail.com>
2024-02-25 22:29:29 +0100

    ADD: Spotify + Spacetify

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, spicetify-nix, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -18,10 +19,11 @@
     {
       homeConfigurations."shamorn" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        extraSpecialArgs = { inherit spicetify-nix; };
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./home.nix ./derivations/spicetify.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
