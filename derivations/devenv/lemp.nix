a18952b4e32e93b4efde63dfade566bdda8323d6
daniele.cosio <daniele.cosio99@gmail.com>
2024-03-26 20:26:58 +0100

    lemp devenv config updated

--- derivations/devenv/lemp.nix
+++ derivations/devenv/lemp.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   projectName = "app";
+  projectRoot = "";
 in
 {
 
@@ -12,8 +13,20 @@ in
   languages.php.package = pkgs.php82.buildEnv {
     extraConfig = ''
       memory_limit = 256m
+      pdo_mysql.default_socket=${projectRoot}/.devenv/mysql.sock
     '';
   };
+  languages.php.fpm.pools.web = {
+    settings = {
+      "pm" = "dynamic";
+      "pm.max_children" = 10;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 1;
+      "pm.max_spare_servers" = 10;
+      "pm.max_requests" = 500;
+    };
+  };
+
 
   services.mysql = {
     enable = true;
@@ -35,16 +48,53 @@ in
       listen 1337;
       listen [::]:1337;
 
-       server_name ${projectName}.dev;
+      server_name ${projectName}.lc;
 
-       root ./public;
-       index index.php index.html index.htm;
+      add_header X-Frame-Options "SAMEORIGIN";
+      add_header X-Content-Type-Options "nosniff";
 
-       location / {
-               try_files $uri $uri/ =404;
+      index index.php;
+
+      charset utf-8;
+
+      error_page 404 /index.php;
+
+      root ${projectRoot}/public;
+      index index.php index.html index.htm;
+
+      location = /favicon.ico { access_log off; log_not_found off; }
+      location = /robots.txt  { access_log off; log_not_found off; }
+
+      location / {
+        try_files $uri $uri/ /index.php?$query_string;
+      }
+
+      location ~ \.php$ {
+        fastcgi_pass unix:/${config.languages.php.fpm.pools.web.socket};
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        include ${config.services.nginx.package}/conf/fastcgi.conf;
+      }
+
+      location ~ /\.ht {
+        deny all;
+      }
+
+      location ~ /\.(?!well-known).* {
+        deny all;
       }
     }
   '';
+
+  enterShell = ''
+    if [[ ! -d vendor ]]; then
+        composer install
+    fi
+    
+    if [[ ! -d node_modules ]]; then
+        yarn
+    fi
+  '';
+
 
   # Project specific MySQL config like require always a primary key
   # services.mysql.settings.mysqld = {
